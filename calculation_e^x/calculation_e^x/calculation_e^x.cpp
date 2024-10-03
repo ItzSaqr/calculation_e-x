37952d1fbba8e6f322fe4000d8f5db0e076636ef
Teriooot <vlassigov3@gmail.com>
2024-10-03 20:26:08 +0700

    Изменён main. За цикл вынесено то, что не должно быть в цикле.

--- calculation_e^x/calculation_e^x/calculation_e^x.cpp
+++ calculation_e^x/calculation_e^x/calculation_e^x.cpp
@@ -1,31 +1,29 @@
 ﻿#include <iostream>
-#include <locale.h>
 #include <quadmath.h>
 #include "../headers/calculation_e^x.h"
 
 int main()
 {
+	string format = "%.31Qe";
+
+	__float128 calculation_result;
+	__float128 x;
+	uint32_t n;
+	uint32_t amount_chars;
+
 	while (true)
 	{
-		setlocale(LC_ALL, "Russian");
-
-		string format = "%.31Qe";
-
-		__float128 calculation_result;
-		__float128 x;
-		uint32_t n;
-
-		cout << "Введите x: ";
+		cout << "Enter x: ";
 		string in;
 		cin >> in;
 		x = strtoflt128(in.c_str(), NULL);
 
-		cout << "Введите n: ";
+		cout << "Enter n: ";
 		cin >> n;
 
 		calculation_ex(x, n, &calculation_result);
 
-		uint32_t amount_chars = (uint32_t)quadmath_snprintf(NULL, 0, format.c_str(), calculation_result);
+		amount_chars = (uint32_t)quadmath_snprintf(NULL, 0, format.c_str(), calculation_result);
 		string out(amount_chars + 1, '\0');
 		quadmath_snprintf(&out[0], out.capacity(), format.c_str(), calculation_result);
 		cout << out << endl;
