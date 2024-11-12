7f83af99e79031905dd8fd0d3f11d62f59c842d5
ItzSaqr <yrik435@mail.ru>
2024-11-12 17:08:01 +0700

    никогда не мойся

--- calculation_e^x/headers/calculation_e^x_optimization.h
+++ calculation_e^x/headers/calculation_e^x_optimization.h
@@ -10,44 +10,39 @@ using namespace chrono;
 
 static void calculation_ex_optimization(__float128 x, uint32_t n, __float128* out, int threads_number)
 {
-    __float128 result = 1;
-    auto time_start = high_resolution_clock::now();
-    bool negative_x = false;
-    omp_set_num_threads(threads_number);
+	__float128 result = 1;
+	auto time_start = high_resolution_clock::now();
+	bool negative_x = false;
+	omp_set_num_threads(threads_number);
 
-    if (x == 0)
-    {
-        result = 1;
-    }
-    else
-    {
-        if (x < 0) {
-            x *= -1;
-            negative_x = true;
-        }
+	if (x == 0)
+	{
+		result = 1;
+	}
+	else
+	{
+		if (x < 0)
+		{
+			x *= -1;
+			negative_x = true;
+		}
 
-        // Используем переменную для хранения промежуточного результата
-        __float128 partial_result = 0;
+#pragma omp parallel for reduction(+:result)
+		for (uint32_t i = n; i > 0; i--)
+		{
+			result *= x / i;
+			result += 1;
+		}
+		result -= threads_number;
+		if (result != INFINITY && negative_x)
+		{
+			result = 1 / result;
+		}
 
-#pragma omp parallel for reduction(+:partial_result)
-        for (uint32_t i = n; i > 0; i--)
-        {
-            __float128 term = (x / i);
-            partial_result += term;
-        }
+		auto time_end = high_resolution_clock::now();
+		duration<double> time = time_end - time_start;
+		cout << "time = " << time.count() << " s" << endl;
+	}
 
-        // Добавляем 1 к результату
-        result += partial_result;
-
-        if (result != INFINITY && negative_x)
-        {
-            result = 1 / result;
-        }
-
-        auto time_end = high_resolution_clock::now();
-        duration<double> time = time_end - time_start;
-        cout << "time = " << time.count() << " s" << endl;
-    }
-
-    *out = result;
+	*out = result;
 }
